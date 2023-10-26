82e193bad0926e1965eeca0da736124c3f69315f
Matheus de Sousa <88354244+mathensousaa@users.noreply.github.com>
2023-10-26 20:45:25 -0300

    Delete node_modules directory

--- node_modules/aurelia-path/dist/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function relativeToFile(name: string, file: string): string;
-export declare function join(path1: string, path2: string): string;
-export declare function buildQueryString(params?: Object, traditional?: boolean): string;
-export declare function parseQueryString(queryString: string): Object;
